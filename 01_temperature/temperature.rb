ecdea333cd7980eee0f55c9a581936509073cca3
manuporto <manu.porto94@gmail.com>
2016-12-11 14:45:23 -0300

    Implemented solutions

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc(t)
+    (5.0/9.0) * (t - 32)
+end
+
+def ctof(t)
+    (9.0/5.0) * t + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
 #write your code here
+def add(a,b)
+    a + b
+end
+
+def subtract(a,b)
+    a - b
+end
+
+def sum(array)
+    res = 0
+    array.each { |i| res += i}
+    res
+end
+
+def multiply(a,b)
+    a * b
+end
+
+def power(a,b)
+    a ** b
+end
+
+def factorial(a)
+    res = 1
+    (1..a).each{|i| res *= i}
+    res
+end
